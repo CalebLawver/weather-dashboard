9f1860c7678925a7536a57a4cab3cf52cdc04246
Richard Caleb Lawver <clawver30@gmail.com>
2022-04-18 16:47:36 -0600

    Starting to work on the js for the website

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,18 @@
-var apiKey = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=1102146e44da3e97c0e180cac7f33405"
+var apiKey = "524901&appid=1102146e44da3e97c0e180cac7f33405";
+var searchHistoryAll = [];
+$("#day1").append(moment().format("L"));
 
-// using imperial units requires &units=imperial
+function currentCondition(city) {
+    var searchUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey;
 
-    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&units=imperial&appid=1102146e44da3e97c0e180cac7f33405"
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    }).then(function(cityWeather) {
+        console.log(cityWeather);
 
-    fetch(apiUrl).then(function(response) {
-        response.json().then(function(data) {
-            console.log(data);
-        });
-    });
+        var iconCode = cityWeather.weather[0].icon;
+        var iconUrl = "https://openweathermap.org/img/w/" + iconCode + ".png";
+    })
+}
 
