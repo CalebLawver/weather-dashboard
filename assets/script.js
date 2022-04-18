ff8fee386f994688bcd3bc2151328c8b3e5deb50
Richard Caleb Lawver <clawver30@gmail.com>
2022-04-18 15:03:03 -0600

    Api key works, working on how to implement now

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,12 @@
 var apiKey = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=1102146e44da3e97c0e180cac7f33405"
+
+// using imperial units requires &units=imperial
+
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&units=imperial&appid=1102146e44da3e97c0e180cac7f33405"
+
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
+
