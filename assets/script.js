a5e24719b67fc387879a73f8a7e5a5c4c1fea24e
Richard Caleb Lawver <clawver30@gmail.com>
2022-04-20 15:02:45 -0600

    Getting data to appear on the site

--- assets/script.js
+++ assets/script.js
@@ -3,14 +3,20 @@ var apiKey = "&appid=1102146e44da3e97c0e180cac7f33405";
 let cityNameInput = document.querySelector("#city");
 let searchedCity = document.querySelector("#user-form");
 let todaysWeather = document.querySelector("#current-day");
-let todaysWeatherH2 = document.querySelector("#current-day h2");
-let todaysWeatherP = document.querySelector("#current-day p");
+let displayedCity = document.querySelector("#search-city");
+let icon = document.querySelector("#weatherIcon");
+let tempCity = document.querySelector("#temp1");
+let windCity = document.querySelector("#wind1");
+let humidCity = document.querySelector("#humid1");
+let uvCity = document.querySelector("#uv");
+
+$("#date").text(moment().format('l'));
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
 
     // IM STUCK HERE
-    var enteredCity = searchedCity.value.trim();
+    var enteredCity = cityNameInput.value.trim();
 
     if (enteredCity) {
         citiesConditions(enteredCity);
@@ -20,7 +26,7 @@ var formSubmitHandler = function(event) {
 };
 
 var citiesConditions = function(city) {
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=logan&units=imperial&appid=1102146e44da3e97c0e180cac7f33405";
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=1102146e44da3e97c0e180cac7f33405";
 
     fetch(apiUrl)
         .then(function(response) {
@@ -39,8 +45,19 @@ var citiesConditions = function(city) {
         });
 };
 
-var currentConditions = function() {
+var currentConditions = function(weather, city) {
+    let currentWeatherIcon = "https://openweathermap.org/img/w/" + weather.weather[0].icon + ".png";
+    console.log(weather.name, currentWeatherIcon, weather.main.temp, weather.main.humidity, weather.wind.speed);
+    $(displayedCity).text(weather.name);
+    $(tempCity).text(weather.main.temp);
+    $(windCity).text(weather.wind.speed);
+    $(humidCity).text(weather.main.humidity);
 
+    // getting weather icon to appear
+    var currentIcon = document.createElement("img");
+    currentIcon.src = currentWeatherIcon;
+    var src = document.getElementById("#date");
+    src.appendChild(currentIcon);
 }
 
 searchedCity.addEventListener("submit", formSubmitHandler);
